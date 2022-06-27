37e69a88af5fce5430c63ef7e6ee5169712374ae
Camila Aguiar <camilaguiar73@gmail.com>
2022-06-27 19:38:27 -0300

    feat: response api
    Co-authored-by: elizenai <elizenai.silva@hotmail.com>
    Co-authored-by: Cahaguiar <camilaguiar73@gmail.com>
    Co-authored-by: karolsontak <karolsontak@gmail.com>
    Co-authored-by: alinerc27 <aline.rosa.cruz@gmail.com>
    Co-authored-by: CassiaCosta <cassia.silva93@hotmail.com>

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
-}
+import React, {useState, useEffect} from 'react';
+import Header from "./components/header/header";
 
-export default App;
+
+export default function CountryStatistics(props) { 
+  
+  const [items, setItems] = useState([]);
+
+  useEffect(function() {
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': 'b37675ed39mshc96be10a276d74ap18e650jsneaf3afd93da5',
+        'X-RapidAPI-Host': 'covid-193.p.rapidapi.com'
+      }
+    };
+    
+    fetch('https://covid-193.p.rapidapi.com/statistics', options)
+      .then(response => response.json())
+      .then(response => {
+        console.log(response)
+        return response
+        })
+      .then(response => setItems[response])
+      .catch(err => console.error(err));
+  }, [])
+  return (
+    <div className="App">  
+      <Header />
+      {items.map(item => <div> {item.toString()}</div>)}
+    </div>
+  )
+}
