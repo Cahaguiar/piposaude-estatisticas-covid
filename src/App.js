555554be27c6249ced6921b781e5e968b3d3d22d
Elizenai Silva <elizenai.silva@hotmail.com>
2022-06-27 22:57:46 -0300

    refactor: ESmodules-API
    Co-authored-by: elizenai <elizenai.silva@hotmail.com>
    Co-authored-by: Cahaguiar <camilaguiar73@gmail.com>
    Co-authored-by: karolsontak <karolsontak@gmail.com>
    Co-authored-by: <alinerc27aline.rosa.cruz@gmail.com>
    Co-authored-by: CassiaCosta <cassia.silva93@hotmail.com>

--- src/App.js
+++ src/App.js
@@ -1,38 +1,30 @@
 import React, {useState, useEffect} from 'react';
+import { getData } from './services';
 //import Header from "./components/header/header";
 
 
 export default function CountryStatistics(props) { 
-  
   const [items, setItems] = useState([]);
 
   useEffect(function() {
-    const options = {
-      method: 'GET',
-      headers: {
-        'X-RapidAPI-Key': 'b37675ed39mshc96be10a276d74ap18e650jsneaf3afd93da5',
-        'X-RapidAPI-Host': 'covid-193.p.rapidapi.com'
-      }
-    };
-    
-    fetch('https://covid-193.p.rapidapi.com/statistics', options)
-      .then((response => response.json()))
-      .then((response => {
-        console.log(response)
-        return response
-        }))
-      .then((response) => {
-        const resposta= response.response;
-        setItems(resposta)
-      })
-      .catch(err => console.error(err));
+    getData()
+    .then((response => response.json()))
+    .then((response => {
+      console.log(response)
+      return response
+      }))
+    .then((response) => {
+       const resposta= response.response;
+      setItems(resposta)
+    })
+     .catch(err => console.error(err));
   }, [])
   return (
     <div className="App">  
       {/* <Header /> */}
       {items.map(item => {
         return (
-          <div>
+          <div key={item.country}>
             <p>Países: {item.country}</p>
             <p>Total de casos: {item.cases.total}</p>
             <p>Novos casos: {item.cases.new}</p>
