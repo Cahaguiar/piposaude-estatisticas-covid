1d8113f96c4daefac2f51daa8fa710c0cd5f47f3
Camila Aguiar <camilaguiar73@gmail.com>
2022-06-28 19:52:26 -0300

    confirmar

--- src/App.js
+++ src/App.js
@@ -1,47 +1,45 @@
 import React, {useState, useEffect} from 'react';
+import { getData } from './services';
 //import Header from "./components/header/header";
 
-
 export default function CountryStatistics(props) { 
-  
   const [items, setItems] = useState([]);
 
   useEffect(function() {
-    const options = {
-      method: 'GET',
-      headers: {
-        'X-RapidAPI-Key': 'b37675ed39mshc96be10a276d74ap18e650jsneaf3afd93da5',
-        'X-RapidAPI-Host': 'covid-193.p.rapidapi.com'
-      }
-    };
-    
-    fetch('https://covid-193.p.rapidapi.com/statistics', options)
-      .then((response => response.json()))
-      .then((response => {
-        console.log(response)
-        return response
-        }))
-      .then((response) => {
-        const resposta= response.response;
-        setItems(resposta)
-      })
-      .catch(err => console.error(err));
+    getData()
+    .then((response => response.json()))
+    .then((response) => {
+      const resposta= response.response;
+      setItems(resposta)
+    })
+    .catch(err => console.error(err));
   }, [])
   return (
     <div className="App">  
       {/* <Header /> */}
-      {items.map(item => {
-        return (
-          <div>
-            <p>Países: {item.country}</p>
-            <p>Total de casos: {item.cases.total}</p>
-            <p>Novos casos: {item.cases.new}</p>
-            <p>Total de óbitos: {item.deaths.total}</p>
-          </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Países</th>
+            <th>Total de Casos</th>
+            <th>Novos Casos</th>
+            <th>Total de Óbitos</th>
+          </tr>
+        </thead>
+        <tbody>
+        {items.map(item => {
+          return (
+          <tr>
+            <td>{item.country}</td>
+            <td>{item.cases.total}</td>
+            <td>{item.cases.new}</td>
+            <td>{item.deaths.total}</td>
+          </tr>
         )
-      }
-      )}
-
+        })}
+        </tbody>
+        </table>
     </div>
   )
 }
+
