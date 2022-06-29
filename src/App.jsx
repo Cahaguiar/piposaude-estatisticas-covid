5eb24c4548a2390950b4be78536be91b1ba7dab3
Elizenai Silva <elizenai.silva@hotmail.com>
2022-06-29 17:03:16 -0300

    feat: add filter DataBrazilState - no comments
    Co-authored-by: elizenai <elizenai.silva@hotmail.com>
    Co-authored-by: Cahaguiar <camilaguiar73@gmail.com>
    Co-authored-by: karolsontak <karolsontak@gmail.com>
    Co-authored-by: <alinerc27aline.rosa.cruz@gmail.com>
    Co-authored-by: CassiaCosta <cassia.silva93@hotmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { getData } from "./services";
-// import { getDataBrazil } from "./services";
 import Header from "./components/header/header.jsx";
 import styles from "./style.css";
 import DataBrazilState from "./components/DataBrazilState";
@@ -8,9 +7,7 @@ import DataBrazilState from "./components/DataBrazilState";
 export default function CountryStatistics(props) {
 
   const [items, setItems] = useState([]);
-  // const [itemsBrazil, setItemsBrazil] = useState([]);
-  // const [itemsSelect, setItemsSelect] = useState([])
-
+  
   useEffect(function () {
     getData()
       .then((response) => response.json())
@@ -21,28 +18,6 @@ export default function CountryStatistics(props) {
       .catch((err) => console.error(err));
   }, []);
 
-  // useEffect(function () {
-  //   getDataBrazil()
-  //     .then((response) => response.json())
-  //     .then((response) => {
-  //       setItemsBrazil(response.data);
-  //       setItemsSelect(response.data);
-      
-  //     })
-  //     .catch((err) => console.error(err));
-  // }, []);
-
-  // const filterStates = (data, uf) => {
-  //   if (!uf) {
-  //     return itemsBrazil
-  //   }
-  //   return data.filter((item) => item.uf === uf)
-  // }
-
-  // const handleChange = (e) => {
-  //   setItemsSelect(filterStates(itemsBrazil, e.target.value))
-    
-  // }
   
   return (
     <>
@@ -71,44 +46,7 @@ export default function CountryStatistics(props) {
           </tbody>
         </table>
       </div>
-
       <DataBrazilState />
-      
-      {/* <label>Selecione um Estado:</label>
-      <select className="select-state" onChange={handleChange}>
-        <option value="">Selecione um Estado</option>
-        {itemsBrazil.map((item) => {
-          return (
-            <option value={item.uf}> {item.state}</option>
-          )
-        }
-        )}
-      </select>
-      
-      <div className="data-Brazil">
-        <table>
-          <thead>
-            <tr>
-              <th>Estado</th>
-              <th>Número de Casos</th>
-              <th>Número de Óbitos</th>
-              <th>Número de Suspeitas</th>
-            </tr>
-          </thead>
-          <tbody>
-            {itemsSelect.map((item) => {
-              return (
-                <tr key={item.state}>
-                  <td> {item.state}</td>
-                  <td> {item.cases}</td>
-                  <td> {item.deaths}</td>
-                  <td> {item.suspects}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div> */}
     </>
   );
 }
